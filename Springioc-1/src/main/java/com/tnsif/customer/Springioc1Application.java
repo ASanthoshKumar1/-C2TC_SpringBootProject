5424371acf0dd0a6a172b7eb6090d79ed88e6682
santh <santh@Santhosh>
2025-09-17 21:30:15 +0530

    Task completed

---
+++ Springioc-1/src/main/java/com/tnsif/customer/Springioc1Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springioc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springioc1Application.class, args);
+	}
+
+}
