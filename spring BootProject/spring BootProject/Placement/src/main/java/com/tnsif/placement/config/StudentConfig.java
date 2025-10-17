c67cf5900d2ea5a667177583715d36328dbbed1f
ASanthoshKumar1 <santhoshasokan568@gmail.com>
2025-10-17 12:34:20 +0530

    Updated one

--- spring BootProject/spring BootProject/Placement/src/main/java/com/tnsif/placement/config/StudentConfig.java
+++ spring BootProject/spring BootProject/Placement/src/main/java/com/tnsif/placement/config/StudentConfig.java
@@ -1,4 +1,4 @@
-package com.tnsif.placement.service.config;
+package com.tnsif.placement.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
